3e9c7d5538a2c227ddca7a841f69768f5e17e094
Tonye Iyalla <tiyalla2015@gmail.com>
2018-09-30 12:21:24 -0400

    Fix loading display

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+document.getElementById("hideAll").style.display = "none";
+
 $(window).on("load", function() {
     // weave your magic here.
+    document.getElementById("hideAll").style.display = "block";
 });
