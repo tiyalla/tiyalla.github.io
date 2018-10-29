c49748646f2b1244e49c8a743ff077d8f3bc1c75
Tonye Iyalla <tiyalla@villanova.edu>
2018-10-28 20:42:29 -0400

    Add hugo site

---
+++ content/contact.md
@@ -0,0 +1,9 @@
++++
+title = "Contact"
+slug = "contact"
+thumbnail = "images/tn.png"
+description = "contact"
++++
+
+
+Want to contact me? Feel free to [email me](mailto:tonyeiyalla@yahoo.com).
--- js/app.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("hideAll").style.display = "none";
-
-$(window).on("load", function() {
-    // weave your magic here.
-    document.getElementById("hideAll").style.display = "block";
-});
